c00ff2bccda503901e1d7ba72e286b8032c8f4b4
root <root@kalipc.home>
2024-06-24 14:20:28 -0600

    add install file

---
+++ install-rayla.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+mkdir /rayla
+
+mkdir /rayla/python
+
+mkdir /rayla/reconnassiance
+
+mkdir /rayla/resource-development
+
+mkdir /rayla/initial-access
+
+mkdir /rayla/execution
+
+mkdir /rayla/persistence
+
+mkdir /rayla/privilege-escalation
+
+mkdir /rayla/defense-evasion
+
+mkdir /rayla/credential-access
+
+mkdir /rayla/discovery
+
+mkdir /rayla/lateral-movement
+
+mkdir /rayla/collection
+
+mkdir /rayla/command-control
+
+mkdir /rayla/exfiltration
+
+mkdir /rayla/impact
+
+mkdir /rayla/credential-access/access-point-scan-reporst
+
+mkdir /rayla/credential-access/cracked-wifi-passwords
+
+mkdir /rayla/credential-access/focused-scan-reports
+
+mkdir /rayla/credential-access/targeted-access-point
+
+pip install python-nmap
+
+cd /
+
+git clone https://github.com/Bryonpectol/rayla.git
